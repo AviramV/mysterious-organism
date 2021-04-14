60196efdc0d7ec275a8a28b3a1aa0abaa945be40
AviramV <aviramv2@gmail.com>
2021-04-14 23:47:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -70,33 +70,57 @@ function pAequorFactory(number, strandArray) {
       const countCandG = this.dna.filter(base => base === 'C' || base === 'G');
       const isMinSixtyPercent = countCandG.length / this.dna.length
       return isMinSixtyPercent >= 0.6;
+    },
+
+// Returns the complementary DNA strand. 
+// 'A's match with 'T's and vice versa. 'C's match with
+// 'G's and vice versa.
+    complementStrand () {
+      let compStrand = []; 
+      this.dna.map(base => {        
+        switch (base) {
+          case 'A': compStrand.push('T');
+          break
+          case 'T': compStrand.push('A');
+          break
+          case 'C': compStrand.push('G');
+          break
+          case 'G': compStrand.push('C');
+          break
+          }
+        });
+      return compStrand;
     }
-  }
-}
+  } // End of returned object
+} // End of factory function
 
 // create 30 instances of pAequor that will likely survive
+
 const instancesToStudy = [];
 const createInstances = instancesAmount => {
   let counter = 1;
   let checkInstance;
   while (counter <= instancesAmount) {
     let checkInstance = pAequorFactory(counter, mockUpStrand());
+    
     if (checkInstance.willLikelySurvive()) {
       instancesToStudy.push(checkInstance);
       counter++;
     }    
   }
+  console.log(`Total created instances: ${instancesToStudy.length}`); 
+  console.log(`All specimens are likely to survive? ${instancesToStudy.every(instance => instance.willLikelySurvive())}`
+  );
+  
   return instancesToStudy;
 }
 
 createInstances(30);
-console.log(`Total created instances: ${instancesToStudy.length}`); 
-console.log(
-  `All specimens are likely to survive? ${instancesToStudy.every(instance => instance.willLikelySurvive())}`
-);
+
 
 /*** Tests ***/ 
-//const test = pAequorFactory(1, mockUpStrand());
+//const test = pAequorFactory(100, mockUpStrand());
+//console.log(test.complementStrand())
 //const test2 = pAequorFactory(2, mockUpStrand());
 //console.log(test.dna);
 //console.log(test.mutate());
@@ -104,3 +128,7 @@ console.log(
 //console.log(`specimen #${test.specimenNum}'s DNA: ${test.dna}'`);
 //console.log(`specimen #${test2.specimenNum}'s DNA: ${test2.dna}'`);
 //console.log(test.willLikelySurvive());
+
+
+
+
